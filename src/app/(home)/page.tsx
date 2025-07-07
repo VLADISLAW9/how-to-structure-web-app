0840174a32e971dc80a399ac6c3b7158eecef803
VLADISLAW9 <duplinskij_v_d@mail.ru>
2025-07-07 23:55:47 +0700

    init project

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,18 @@
 import Link from 'next/link';
 
-export default function HomePage() {
+const HomePage = () => {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
+    <main className='flex flex-1 flex-col justify-center text-center'>
+      <h1 className='mb-4 text-2xl font-bold'>Hello World</h1>
+      <p className='text-fd-muted-foreground'>
         You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
+        <Link href='/docs' className='text-fd-foreground font-semibold underline'>
           /docs
         </Link>{' '}
         and see the documentation.
       </p>
     </main>
   );
-}
+};
+
+export default HomePage;
--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,17 +1,24 @@
-import { source } from '@/lib/source';
-import {
-  DocsPage,
-  DocsBody,
-  DocsDescription,
-  DocsTitle,
-} from 'fumadocs-ui/page';
-import { notFound } from 'next/navigation';
 import { createRelativeLink } from 'fumadocs-ui/mdx';
+import { DocsBody, DocsDescription, DocsPage, DocsTitle } from 'fumadocs-ui/page';
+import { notFound } from 'next/navigation';
+
+import { source } from '@/lib/source';
 import { getMDXComponents } from '@/mdx-components';
 
-export default async function Page(props: {
-  params: Promise<{ slug?: string[] }>;
-}) {
+export const generateStaticParams = async () => source.generateParams();
+
+export const generateMetadata = async (props: { params: Promise<{ slug?: string[] }> }) => {
+  const params = await props.params;
+  const page = source.getPage(params.slug);
+  if (!page) notFound();
+
+  return {
+    title: page.data.title,
+    description: page.data.description
+  };
+};
+
+const Page = async (props: { params: Promise<{ slug?: string[] }> }) => {
   const params = await props.params;
   const page = source.getPage(params.slug);
   if (!page) notFound();
@@ -19,34 +26,18 @@ export default async function Page(props: {
   const MDXContent = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage full={page.data.full} toc={page.data.toc}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
         <MDXContent
           components={getMDXComponents({
-            // this allows you to link to other pages with relative file paths
-            a: createRelativeLink(source, page),
+            a: createRelativeLink(source, page)
           })}
         />
       </DocsBody>
     </DocsPage>
   );
-}
+};
 
-export async function generateStaticParams() {
-  return source.generateParams();
-}
-
-export async function generateMetadata(props: {
-  params: Promise<{ slug?: string[] }>;
-}) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
-  if (!page) notFound();
-
-  return {
-    title: page.data.title,
-    description: page.data.description,
-  };
-}
+export default Page;
